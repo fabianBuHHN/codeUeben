be1b3c19a296c63faa12dc5bc23710798b2455ed
Fabianb <90683433+fabianBuHHN@users.noreply.github.com>
2022-04-23 12:50:29 +0200

    Test

---
+++ src/de/hhn/prog2/lab03/controller/Controller.java
@@ -0,0 +1,12 @@
+package de.hhn.prog2.lab03.controller;
+
+import de.hhn.prog2.lab03.view.PizzaConfigPanel;
+
+public class Controller {
+
+    private PizzaConfigPanel configPanel;
+
+    public Controller() {
+        configPanel = new PizzaConfigPanel();
+    }
+}
