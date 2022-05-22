2e95c36d8bc727a6fb0c94bd58bede4ac7fc7ff3
fabutter <90683433+fabianBuHHN@users.noreply.github.com>
2022-05-22 18:51:28 +0200

    lab 07 finished

---
+++ src/de/hhn/prog2/lab07/AnimalZoo.java
@@ -0,0 +1,48 @@
+package de.hhn.prog2.lab07;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+interface Animal {
+    void eat();
+}
+
+class Tiger implements Animal {
+    public void eat() {
+        System.out.println("crunch crunch, roaaaar!");
+    }
+}
+
+class Pig implements Animal {
+    public void eat() {
+        System.out.println("munch munch, oink!");
+    }
+}
+
+public class AnimalZoo {
+    public static void main(String[] args) {
+        ArrayList<Tiger> tigers = new ArrayList<>();
+        tigers.add(new Tiger());
+
+        ArrayList<Pig> pigs = new ArrayList<>();
+        pigs.add(new Pig());
+
+        feed(tigers);
+
+
+         feed(pigs);
+    }
+
+    /**
+     * Calls method eat() for every object
+     * as long as it implements Animal interface.
+     *
+     * @param animals Generic collection which implements Animal class
+     */
+    public static void feed(Collection<? extends Animal> animals) {
+        for (Animal animal : animals) {
+            animal.eat();
+        }
+    }
+
+}
---
+++ src/de/hhn/prog2/lab07/GenericsExercise.java
@@ -0,0 +1,24 @@
+package de.hhn.prog2.lab07;
+
+public class GenericsExercise {
+
+
+    /**
+     * Uses methods from java standard library to display the datatype
+     * which was handed over in parameter list
+     */
+    public <Out> void checkType(Out type) {
+        System.out.format("%s is of type " + type.getClass().getName() + "\r\n", type);
+    }
+
+    public static void main(String[] args) {
+
+        GenericsExercise g = new GenericsExercise();
+
+        g.checkType("Hello");
+        g.checkType(41);
+        g.checkType("A");
+        g.checkType(3.1337);
+
+    }
+}
